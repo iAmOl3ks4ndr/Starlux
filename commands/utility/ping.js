765dece47705a4ab0234759e77709e70212d562c
iAmOl3ks4ndr <o.oleksandr.m@gmail.com>
2025-02-20 12:37:08 +0000

    Links, waits, and edit, defer, follow-up, delete responses

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,3 +1,4 @@
+const wait = require('node:timers/promises').setTimeout;
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
@@ -5,6 +6,9 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply('Pong!');
+		await interaction.deferReply();
+		await wait(4_000);
+		await interaction.editReply({ content: 'Pong!', withResponse: true });
+		await interaction.followUp('Pong again! [Link](http://www.google.com)');
 	},
 };
--- commands/utility/user.js
+++ commands/utility/user.js
@@ -1,3 +1,4 @@
+const wait = require('node:timers/promises').setTimeout;
 const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
@@ -5,6 +6,9 @@ module.exports = {
 		.setName('user')
 		.setDescription('Provides information about the user.'),
 	async execute(interaction) {
-		await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+		const response = await interaction.reply({ content: `This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`, withResponse: true });
+		console.log(response.resource.message);
+		await wait(3_500);
+		await interaction.deleteReply();
 	},
 };
